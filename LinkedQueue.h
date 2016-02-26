e0fc77771697891ecf6e2394b786314f2378577a
EMILY JACKSON <emily@nike.cs.uga.edu>
2016-02-26 14:03:31 -0500

    2/26/2016 2:03 PM

---
+++ LinkedQueue.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <Queue.h>
+#include <LinkedList.h>
+
+struct Node {
+	int data;
+	struct Node* next;
+};
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+void Enqueue(int x) {
+	struct Node* temp = 
+		(struct Node*)malloc(sizeof(struct Node));
+	temp->data =x; 
+	temp->next = NULL;
+	if(front == NULL && rear == NULL){
+		front = rear = temp;
+		return;
+	}
+	rear->next = temp;
+	rear = temp;
+}
+
+void Dequeue() {
+	struct Node* temp = front;
+	if(front == NULL) {
+		printf("Queue is Empty\n");
+		return;
+	}
+	if(front == rear) {
+		front = rear = NULL;
+	}
+	else {
+		front = front->next;
+	}
+	free(temp);
+}
+
+int Front() {
+	if(front == NULL) {
+		printf("Queue is empty\n");
+		return;
+	}
+	return front->data;
+}
+
